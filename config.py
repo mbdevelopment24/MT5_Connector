5f8e1d777453a9a46003b349d069346c09eba5fd
mbdevelopment24 <159246246+mbdevelopment24@users.noreply.github.com>
2024-09-23 08:03:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 """
 Telegram bot configuration
 """
-TELEGRAM_TOKEN = '6405881837:AAFtZy4KfSwfqlCcA2q4nF8K30FXfeUMOX4'
-CHANNEL_USERNAME = '-1002082809410'
+TELEGRAM_TOKEN = 'Token'
+CHANNEL_USERNAME = 'ID'
 
 """
 MetaTrader 5 configuration
 """
-MT5_LOGIN = 1510023458
-MT5_SERVER = 'FTMO-Demo'
-MT5_PASSWORD = '?=?rd@X6F=4'
-MT5_PATH = 'C:\\Program Files\\MetaTrader 5\\terminal64.exe'
+MT5_LOGIN = USER
+MT5_SERVER = 'Server'
+MT5_PASSWORD = 'PASS'
+MT5_PATH = 'PATH'
 
 """
 Order configuration
